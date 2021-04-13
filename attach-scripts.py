9ab4ad0326c6efece582a12542569d8534f8aad8
mikeAdamss <mikelovesbooks@gmail.com>
2021-04-13 21:09:50 +0100

    more stuff

--- attach-scripts.py
+++ attach-scripts.py
@@ -1,4 +1,5 @@
 
+new_pipfile_lines = []
 with open("Pipfile") as f:
 
     # find out where the [requires] bit of the pipfile starts
@@ -6,14 +7,15 @@ with open("Pipfile") as f:
         if "[requires]" in line:
             req_line = i-1
 
-    new_pipfile_lines = []
+with open("Pipfile") as f:
     for i, line in enumerate(f):
         if i == req_line:
+            new_pipfile_lines.append("\n")
             with open("./convenience/scripts") as f2:
                 for line2 in f2:
                     new_pipfile_lines.append(line2)
         new_pipfile_lines.append(line)
 
-with open("Pipfile2") as f:
+with open("Pipfile2", "w") as f:
     for line in new_pipfile_lines:
-        f.write(line, "\n")
+        f.write(line)
