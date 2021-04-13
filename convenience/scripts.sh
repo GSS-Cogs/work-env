a089da87f6dc3489d42bd735c09b3c339a06a9c0
Michael Adams <mikelovesbooks@gmail.com>
2021-04-13 18:52:17 +0100

    Update scripts.sh

--- convenience/scripts.sh
+++ convenience/scripts.sh
@@ -1,1 +1,3 @@
-
+# Update with any local only dependencies
+curl https://raw.githubusercontent.com/mikeAdamss/work-env/main/convenience/scripts.sh?token=ADQULFOZN73UHF6WFHBW6X3AP4KHS -o requirements.txt
+pipenv install -r requirements.txt
