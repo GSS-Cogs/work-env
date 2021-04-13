0848c5611bfa80476e4cece5d9a380f7ee5f8e55
mikeAdamss <mikelovesbooks@gmail.com>
2021-04-13 21:44:24 +0100

    pathing for Pipenv rewrite still not working

--- attach-scripts.py
+++ attach-scripts.py
@@ -1,21 +1,26 @@
 
+import os
+this_path = os.path.dirname(os.path.realpath(__file__))
+
+print("trying", this_path + "/my-work-environment/Pipfile")
 new_pipfile_lines = []
-with open("~/.work-env/my-work-environment/Pipfile") as f:
+with open(this_path + "/my-work-environment/Pipfile") as f:
 
     # find out where the [requires] bit of the pipfile starts
     for i, line in enumerate(f):
         if "[requires]" in line:
             req_line = i-1
 
-with open("~/.work-env/my-work-environment/Pipfile") as f:
+with open(this_path + "/my-work-environment/Pipfile") as f:
     for i, line in enumerate(f):
         if i == req_line:
             new_pipfile_lines.append("\n")
-            with open("./convenience/scripts") as f2:
+            with open(this_path + "/convenience/scripts") as f2:
                 for line2 in f2:
                     new_pipfile_lines.append(line2)
         new_pipfile_lines.append(line)
 
-with open("~/.work-env/my-work-environment/Pipfile", "w") as f:
+with open(this_path + "/my-work-environment/Pipfile", "w") as f:
     for line in new_pipfile_lines:
         f.write(line)
+
--- sync.sh
+++ sync.sh
@@ -1,9 +1,10 @@
 echo Activating virtual env
 start_dir=$(PWD)
 mkdir -p ~/.work-env/my-work-environment
-cd ~/.work-env
-curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile -o ~/my-work-environment/Pipfile
-curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile.lock -o ~/my-work-environment/Pipfile.lock
-python3 $PWD/attach-scripts.py
+cd ~/.work-env/my-work-environment
+git pull
+curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile -o ~/.work-env/my-work-environment/Pipfile
+curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile.lock -o ~/.work-env/my-work-environment/Pipfile.lock
+python3  ~/.work-env/attach-scripts.py
 pipenv install
 pipenv shell cd $start_dir
