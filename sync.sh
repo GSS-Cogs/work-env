0196e986a261488ce0026621c184e72c79773153
mikeAdamss <mikelovesbooks@gmail.com>
2021-04-14 08:43:31 +0100

    tidy up sync

--- sync.sh
+++ sync.sh
@@ -5,5 +5,7 @@ cd ~/.work-env/my-work-environment
 git pull
 curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile -o ~/.work-env/my-work-environment/Pipfile
 curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile.lock -o ~/.work-env/my-work-environment/Pipfile.lock
+export PIPENV_PIPFILE=~/.work-env/my-work-environment/Pipfile
 pipenv install
+pipenv install -r ~/.work-env/additional-requirements.txt
 pipenv shell "cd $start_dir"
