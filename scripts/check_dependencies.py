1404d531bb213716b3409617aa11c2db4bd307f0
Michael Adams <mikelovesbooks@gmail.com>
2021-04-15 09:30:00 +0100

    abort on a failed get

--- scripts/check_dependencies.py
+++ scripts/check_dependencies.py
@@ -5,7 +5,7 @@ import requests
 
 r = requests.get("https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile.lock")
 if r.status_code != 200:
-    print('Unable to get lock file for dependencies. Aborting.')
+    print('Unable to get lock file for dependencies. Aborting check.')
 else:
 
     lock = r.json()
@@ -23,11 +23,11 @@ else:
         if req.startswith("databaker"):
             databaker_is = req.split("@")[2].strip()
 
-print(f'gss-utils commit is:', gss_utils_is, 'should be:', gssutils_should_be)
-print(f'databaker commit is:', databaker_is, 'should be:', databaker_should_be)
+    print(f'gss-utils commit is:', gss_utils_is, 'should be:', gssutils_should_be)
+    print(f'databaker commit is:', databaker_is, 'should be:', databaker_should_be)
 
-if databaker_is != databaker_should_be or gssutils_should_be != gss_utils_is:
-    for _ in range(20):
-        print('Youre dependencies are out of date, you need to run the "sync" command!')
-else:
-    print("You Dependencies are up to date.")
+    if databaker_is != databaker_should_be or gssutils_should_be != gss_utils_is:
+        for _ in range(20):
+            print('Youre dependencies are out of date, you need to run the "sync" command!')
+    else:
+        print("You Dependencies are up to date.")
