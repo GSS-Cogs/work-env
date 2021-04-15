24f893c665810ec13a155f7716c554b0472acd9e
Michael Adams <mikelovesbooks@gmail.com>
2021-04-15 09:34:01 +0100

    git pull the right repo

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 echo Activating virtual env
 start_dir=$(PWD)
 mkdir -p ~/.work-env/my-work-environment
-cd ~/.work-env/my-work-environment
+cd ~/.work-env
 git pull
 curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile -o ~/.work-env/my-work-environment/Pipfile
 curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile.lock -o ~/.work-env/my-work-environment/Pipfile.lock
