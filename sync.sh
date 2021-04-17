c0c0d6487b01b4b90fcaf5118a22d32adf623983
mikeAdamss <mikelovesbooks@gmail.com>
2021-04-17 14:48:17 +0100

    make sure the update happens

--- sync.sh
+++ sync.sh
@@ -1,10 +1,13 @@
 echo Activating virtual env
 start_dir=$(PWD)
 mkdir -p ~/.work-env/my-work-environment
+cd ~/.work-env/my-work-environment
+pipenv --rm
 cd ~/.work-env
 git pull
 curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile -o ~/.work-env/my-work-environment/Pipfile
 curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile.lock -o ~/.work-env/my-work-environment/Pipfile.lock
 export PIPENV_PIPFILE=~/.work-env/my-work-environment/Pipfile
+export PIP_NO_CACHE_DIR=true
 pipenv install
 pipenv install -r ~/.work-env/additional-requirements.txt
