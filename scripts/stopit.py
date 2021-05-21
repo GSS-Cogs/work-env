99f883cdb2d0d258fe1023e74766565156985140
Michael Adams <mikelovesbooks@gmail.com>
2021-05-21 08:35:55 +0100

    Update stopit.py

--- scripts/stopit.py
+++ scripts/stopit.py
@@ -24,7 +24,7 @@ if not JENKINS_API_TOKEN or not JENKINS_API_TOKEN:
     You can get both by clicking your user name in the top right hand corner
     after logging into our jenkins server (click vredentials to set an api token).
 
-    Export them on teh command line via "export JENKINS_USER=<your username" etc
+    Export them on the command line via "export JENKINS_USER=<your username" etc
     or add them to ~/.bashrc or ~/.zshrc and restarrt yor terminal.
     ''')
 
