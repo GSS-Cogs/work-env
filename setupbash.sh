5d6228116a3a9beb3bece331baa0af1ee241bde7
Michael Adams <mikelovesbooks@gmail.com>
2021-07-22 13:58:03 +0100

    Update setupbash.sh

--- setupbash.sh
+++ setupbash.sh
@@ -8,4 +8,4 @@ echo "" >> ~/.bashrc
 echo "# For Work-env" >> ~/.bashrc
 echo "alias enter=~/.work-env/enterbash.sh"  >> ~/.bashrc
 echo "alias sync=~/.work-env/sync.sh" >> ~/.bashrc
-echo `alias cli="python3 ~/.work-env/scripts/cli.py"` >> ~bashrc
+echo "alias cli=\"python3 ~/.work-env/scripts/cli.py\"" >> ~bashrc
