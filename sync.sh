9d048ce24dade3bcaa1335924ecc6724bf1209f8
Michael Adams <mikelovesbooks@gmail.com>
2021-07-23 08:43:28 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -9,7 +9,6 @@ git pull
 curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile -o ~/.work-env/my-work-environment/Pipfile
 curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile.lock -o ~/.work-env/my-work-environment/Pipfile.lock
 export PIPENV_PIPFILE=~/.work-env/my-work-environment/Pipfile
-export PIP_NO_CACHE_DIR=true
 pipwnv install "wheel"
 pipenv install --skip-lock
 pipenv run python3 ~/.work-env/scripts/extend_environment.py
