a8c58c374f29efbda37ba2ed348a6463b908759e
mikeAdamss <mikelovesbooks@gmail.com>
2021-09-03 09:54:52 +0100

    speed it all up a bit

--- sync.sh
+++ sync.sh
@@ -9,7 +9,10 @@ git pull
 curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile -o ~/.work-env/my-work-environment/Pipfile
 curl https://raw.githubusercontent.com/GSS-Cogs/databaker-docker/master/Pipfile.lock -o ~/.work-env/my-work-environment/Pipfile.lock
 export PIPENV_PIPFILE=~/.work-env/my-work-environment/Pipfile
-pipenv install "wheel"
-pipenv sync
+echo Adding missing wheel where needed
+pipenv run pip install wheel
+echo Installing dependencies from databaker-docker
+pipenv sync --dev
+echo Installing any additional dependencies
 pipenv run python3 ~/.work-env/scripts/extend_environment.py
-pipenv install -r ~/.work-env/additional-requirements.txt
+pipenv install -r ~/.work-env/additional-requirements.txt --skip-lock
